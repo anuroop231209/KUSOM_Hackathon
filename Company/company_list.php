b939db7d3920b833eadd8f407d0233664b31a9f2
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2024-06-15 15:09:20 +0545

    ss

--- Company/company_list.php
+++ Company/company_list.php
@@ -4,24 +4,88 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRM Accounts</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+ <link rel="stylesheet" href="../Sidebar/styles.css">
+ <style>
+            body{
+                font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                color: #202557;
+            }
+            table {
+                width: 80%;
+                border-collapse: collapse;
+                background-color: #F5F5F5;
+            }
+            th, td {
+                padding: 10px;
+                border: 1px solid lightgray;
+                text-align: left;
+            }
+            
+.sidebar {
+    height: 100%;
+    width: 250px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #202557;
+    padding-top: 20px;
+    color: white;
+}
+
+.sidebar h2 {
+    text-align: center;
+    margin-bottom: 30px;
+}
+
+.sidebar ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+.sidebar ul li {
+    padding: 8px;
+    text-align: center;
+}
+
+.sidebar ul li a {
+    display: block;
+    color: white;
+    text-decoration: none;
+    padding: 10px 0;
+}
+
+.sidebar ul li a.active,
+.sidebar ul li a:hover {
+    background-color: #1c2689 ;
+    color: white;
+}
+
+.main{
+    margin-left: 300px;
+}
+</style>
 </head>
-<body class="h-full bg-light">
+<body id="body-pd" >
+<?php
+include_once("../Sidebar/sidebar.html");
+?>    
 <h2>CRM Accounts</h2>
-<div class='table-responsive'>
+<div class='main'>
     <table class='table table-bordered table-sm'>
         <thead class='thead-light'>
         <tr>
-            <th class='p-2'>Id</th>
-            <th class='p-2'>Name</th>
-            <th class='p-2'>Email</th>
-            <th class='p-2'>Phone</th>
-            <th class='p-2'>Address</th>
-            <th class='p-2'>Actions</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Contact Person</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Landline Number</th>
+            <th>Address</th>
+            <th>URL</th>
+            <th>Actions</th>
         </tr>
         </thead>
-        <tbody>
+        <tbody id="databody">
         <?php
         include_once("../Config/config.php");
         include_once("../API/Fetch/fetch_company.php");
