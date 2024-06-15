ce1d49438fdc36019aa586066b2de62ac3edfd08
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2024-06-15 15:54:02 +0545

    new

--- Customer/add-customer.php
+++ Customer/add-customer.php
@@ -6,11 +6,14 @@
     <title>User Registration</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Karla&display=swap">
     <link rel="stylesheet" href="customer.css">
+    <link rel="stylesheet" href="../Sidebar/styles.css">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="script.js"></script>
 </head>
-<body>
-    
+<body id="body-pd">
+<?php
+include_once("../Sidebar/sidebar.html");
+?> 
     <div class="container">
         <form id="registrationForm" action="../API/Insert/insert_customer.php" method="post">
             <h2>Add Customer</h2>
@@ -46,5 +49,8 @@
             <button type="submit">Register</button>
         </form>
     </div>
+    <script src="../Sidebar/main.js"></script>
+<script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+</body>
 </body>
 </html>
